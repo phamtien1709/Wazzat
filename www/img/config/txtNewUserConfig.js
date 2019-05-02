0789ad16392eecd18f24dc68c00a249c7a4cea49
Pham Tien <phamtien1709@gmail.com>
2019-05-02 10:20:24 +0700

    update language

--- www/img/config/txtNewUserConfig.js
+++ www/img/config/txtNewUserConfig.js
@@ -1,5 +1,12 @@
 export default {
-    "userBeGiven3Playlist": "Bạn được tặng 6 Playlist Mới",
-    "userChooseGenresLeft": "Hãy chọn",
-    "userChooseGenresRight": "thể loại nhạc mà bạn yêu thích"
+    "en": {
+        "userBeGiven3Playlist": "Bạn được tặng 6 Playlist Mới",
+        "userChooseGenresLeft": "Hãy chọn",
+        "userChooseGenresRight": "thể loại nhạc mà bạn yêu thích",
+    },
+    "vi": {
+        "userBeGiven3Playlist": "Bạn được tặng 6 Playlist Mới",
+        "userChooseGenresLeft": "Hãy chọn",
+        "userChooseGenresRight": "thể loại nhạc mà bạn yêu thích"
+    }
 }
